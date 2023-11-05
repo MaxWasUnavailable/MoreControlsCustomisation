5104ccad2d3dc47ec8c180fca9fab340eb45e8bc
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2023-11-05 19:41:32 +0100

    Rework to patch PlayerControllerB instead

--- MoreControlsCustomisation/Patches/PatchInitializeGame.cs
+++ MoreControlsCustomisation/Patches/PatchInitializeGame.cs
@@ -1,52 +1,35 @@
+using GameNetcodeStuff;
 using HarmonyLib;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace MoreControlsCustomisation.Patches;
 
 /// <summary>
-///     Harmony patches for the <c>InitializeGame</c> class.
+///     Harmony patches for the <c>PlayerControllerB</c> class.
 /// </summary>
-[HarmonyPatch(typeof(InitializeGame))]
+[HarmonyPatch(typeof(PlayerControllerB))]
 public class PatchInitializeGame
 {
     /// <summary>
-    ///     Patch the <c>InitializeGame</c> Awake method.
+    ///     Patch the <c>PlayerControllerB</c> CalculateSmoothLookingInput method.
     /// </summary>
-    /// <param name="__instance"><c>InitializeGame</c> instance</param>
-    [HarmonyPatch("Awake")]
-    [HarmonyPostfix]
-    public static void InvertControls(InitializeGame __instance)
+    /// <param name="inputVector"><c>Vector2</c> instance with the input vector.</param>
+    [HarmonyPatch("CalculateSmoothLookingInput")]
+    [HarmonyPrefix]
+    public static void InvertControlsCalculateSmoothLookingInput(ref Vector2 inputVector)
     {
-        Plugin.Logger.LogInfo("Patching InitializeGame...");
+        inputVector.y *= Plugin.Instance.IsInvertYAxis.Value ? -1 : 1;
+    }
 
-        var playerActions = __instance.playerActions;
-
-        var m_Movement_Look =
-            Traverse.Create(playerActions).Field<InputAction>("m_Movement_Look"); // Movement/Look[/Mouse/delta]
-        var m_Movement_SwitchItem =
-            Traverse.Create(playerActions)
-                .Field<InputAction>("m_Movement_SwitchItem"); // Movement/SwitchItem[/Mouse/scroll/y]
-        var applyBindingOverrideMethod =
-            typeof(InputAction).GetMethod("ApplyBindingOverride", new[] { typeof(InputBinding) });
-
-        Plugin.Logger.LogDebug("Applying binding overrides...");
-
-        // If Y axis inverted, apply binding override
-        if (Plugin.Instance.IsInvertYAxis.Value)
-        {
-            Plugin.Logger.LogDebug("Inverting Y axis...");
-
-            applyBindingOverrideMethod.Invoke(m_Movement_Look,
-                new object[] { new InputBinding { overrideProcessors = "invertVector2(invertX=false,invertY=true)" } });
-
-            Plugin.Logger.LogDebug("Y axis inverted!");
-        }
-
-        // If scroll direction inverted, apply binding override
-        if (Plugin.Instance.IsInvertScrollDirection.Value)
-            applyBindingOverrideMethod.Invoke(m_Movement_SwitchItem,
-                new object[] { new InputBinding { overrideProcessors = "invertVector2(invertX=false,invertY=true)" } });
-
-        Plugin.Logger.LogInfo("Patch applied!");
+    /// <summary>
+    ///     Patch the <c>PlayerControllerB</c> CalculateNormalLookingInput method.
+    /// </summary>
+    /// <param name="inputVector"><c>Vector2</c> instance with the input vector.</param>
+    [HarmonyPatch("CalculateNormalLookingInput")]
+    [HarmonyPrefix]
+    public static void InvertControlsCalculateNormalLookingInput(ref Vector2 inputVector)
+    {
+        inputVector.y *= Plugin.Instance.IsInvertYAxis.Value ? -1 : 1;
     }
 }
