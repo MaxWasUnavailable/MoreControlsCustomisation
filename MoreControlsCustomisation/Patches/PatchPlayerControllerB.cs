f7324897c2f6622d9f7131310bfe9a7dea1dadaa
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2023-11-06 03:40:42 +0100

    Fix transpiler patch inverting scrolling

--- MoreControlsCustomisation/Patches/PatchPlayerControllerB.cs
+++ MoreControlsCustomisation/Patches/PatchPlayerControllerB.cs
@@ -3,24 +3,16 @@ using System.Reflection.Emit;
 using GameNetcodeStuff;
 using HarmonyLib;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace MoreControlsCustomisation.Patches;
 
-// public static class PatchPlayerControllerBHelpers
-// {
-//     public static double ConditionalInvertScrollAmount(double scrollAmount)
-//     {
-//         Plugin.Logger.LogInfo($"Scroll amount: {scrollAmount}");
-//         if (!Plugin.Instance.IsInvertScrollDirection.Value) return scrollAmount;
-//
-//         scrollAmount *= -1;
-//         Plugin.Logger.LogInfo($"Inverted scroll amount: {scrollAmount}");
-//
-//
-//         return scrollAmount;
-//     }
-// }
+public static class PatchPlayerControllerBHelpers
+{
+    public static double ConditionalInvertScrollAmount(double scrollAmount)
+    {
+        return Plugin.Instance.IsInvertScrollDirection.Value ? -scrollAmount : scrollAmount;
+    }
+}
 
 /// <summary>
 ///     Harmony patches for the <c>PlayerControllerB</c> class.
@@ -50,55 +42,42 @@ public class PatchPlayerControllerB
         inputVector.y *= Plugin.Instance.IsInvertYAxis.Value ? -1 : 1;
     }
 
-    // /// <summary>
-    // ///     Patch the <c>PlayerControllerB</c> SwitchItem_performed method.
-    // /// </summary>
-    // /// <param name="instructions">An <c>IEnumerable</c> of <c>CodeInstruction</c> instances.</param>
-    // [HarmonyPatch("SwitchItem_performed")]
-    // [HarmonyTranspiler]
-    // public static IEnumerable<CodeInstruction> InvertControlsSwitchItemPerformed(
-    //     IEnumerable<CodeInstruction> instructions)
-    // {
-    //     var callVirtCount = 0;
-    //     var writeNewCode = false;
-    //     var skipNextX = 0;
-    //
-    //     foreach (var instruction in instructions)
-    //     {
-    //         if (writeNewCode)
-    //         {
-    //             // First we store the value of the scroll wheel in a local variable
-    //             yield return new CodeInstruction(OpCodes.Ldarga_S, 1);
-    //             yield return new CodeInstruction(OpCodes.Call,
-    //                 AccessTools.Method(typeof(InputAction.CallbackContext),
-    //                     nameof(InputAction.CallbackContext.ReadValue)));
-    //             // yield return new CodeInstruction(OpCodes.Stloc_S, "scrollWheelValue");
-    //
-    //             // Next line is this: if ((double) context.ReadValue<float>() > 0.0)
-    //             // We need to replace this with: if (ConditionalInvertScrollAmount(scrollWheelValue) > 0.0)
-    //             // yield return new CodeInstruction(OpCodes.Ldloc_S, "scrollWheelValue");
-    //             // yield return new CodeInstruction(OpCodes.Call,
-    //             //     AccessTools.Method(typeof(PatchPlayerControllerBHelpers), nameof(PatchPlayerControllerBHelpers.ConditionalInvertScrollAmount)));
-    //
-    //             // We leave the > 0.0 and branch instructions as they are, skipping just the initial two instructions that load context and call ReadValue<float>()
-    //
-    //             writeNewCode = false;
-    //             skipNextX = 2;
-    //         }
-    //
-    //         if (instruction.opcode == OpCodes.Callvirt && callVirtCount < 4)
-    //         {
-    //             callVirtCount++;
-    //             if (callVirtCount == 4) writeNewCode = true;
-    //         }
-    //
-    //         if (skipNextX > 0)
-    //         {
-    //             skipNextX--;
-    //             continue;
-    //         }
-    //
-    //         yield return instruction;
-    //     }
-    // }
+    /// <summary>
+    ///     Patch the <c>PlayerControllerB</c> SwitchItem_performed method.
+    /// </summary>
+    /// <param name="instructions">An <c>IEnumerable</c> of <c>CodeInstruction</c> instances.</param>
+    [HarmonyPatch("SwitchItem_performed")]
+    [HarmonyTranspiler]
+    public static IEnumerable<CodeInstruction> InvertControlsSwitchItemPerformed(
+        IEnumerable<CodeInstruction> instructions)
+    {
+        var callVirtCount = 0;
+        var writeAfterX = -1;
+
+        foreach (var instruction in instructions)
+        {
+            if (writeAfterX >= 0)
+            {
+                if (writeAfterX == 0)
+                {
+                    // We have (double) context.ReadValue<float>() in the stack as following instructions:
+                    //  IL_00c9: ldarga.s     context (might actually be 1 instead of context)
+                    //  IL_00cb: call         instance !!0/*float32*/ [Unity.InputSystem]UnityEngine.InputSystem.InputAction/CallbackContext::ReadValue<float32>()
+
+                    // Next, we use it in the original if statement: if (ConditionalInvertScrollAmount(value on stack) > 0.0)
+                    yield return new CodeInstruction(OpCodes.Call,
+                        AccessTools.Method(typeof(PatchPlayerControllerBHelpers), nameof(PatchPlayerControllerBHelpers.ConditionalInvertScrollAmount)));
+                };
+                writeAfterX--;
+            }
+
+            if (instruction.opcode == OpCodes.Callvirt && callVirtCount < 4)
+            {
+                callVirtCount++;
+                if (callVirtCount == 4) writeAfterX = 2;
+            }
+            
+            yield return instruction;
+        }
+    }
 }
